7a3fd751f355cc290afbceb01d7179fc51f5fdeb
thanhdatho0 <thanhdatho0@gmail.com>
2023-11-07 22:41:36 +0700

    1.0

--- javaScript/header.js
+++ javaScript/header.js
@@ -1,5 +1,5 @@
 // Change header height when scroll start
-window.onscroll = function(){scrollFunction(), 5000};
+window.onscroll = function(){scrollFunction(), addBoxShadow()};
 
 function scrollFunction(){
     if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
@@ -7,6 +7,15 @@ function scrollFunction(){
     }
     else{
         document.getElementById("header").style.padding = "45px 40px";
+    }
+}
+
+function addBoxShadow(){
+    if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
+        document.getElementById("header").style.boxShadow = "1px 1px 5px #ccc"
+    }
+    else{
+        document.getElementById("header").style.boxShadow = "none";
     }
 }
 // Change header height when scroll end
