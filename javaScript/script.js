53aaf00613767908e6c1a6520b1dcd07f126b70c
thanhdatho0 <thanhdatho0@gmail.com>
2023-11-09 15:58:23 +0700

    change

---
+++ javaScript/script.js
@@ -0,0 +1,56 @@
+// Change header height when scroll start
+window.onscroll = function(){scrollFunction(), addBoxShadow(),fixed_content()};
+
+function scrollFunction(){
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+        document.getElementById("header").style.padding = "20px 40px";
+    }
+    else{
+        document.getElementById("header").style.padding = "45px 40px";
+    }
+}
+
+function addBoxShadow(){
+    if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
+        document.getElementById("header").style.boxShadow = "1px 1px 5px #ccc"
+    }
+    else{
+        document.getElementById("header").style.boxShadow = "none";
+    }
+}
+// Change header height when scroll end
+
+// Auto slide navigation function start
+var count = 1;
+setInterval(function(){
+    document.getElementById('radio' + count).checked = true;
+    count++;
+    if(count > 3){
+        count = 1;
+    }
+}, 4000);
+// Auto slide navigation function end
+
+
+function plus() {
+    if (Number(document.getElementById('vd1').textContent) >= 1) {
+        document.getElementById('vd1').innerHTML++;
+    }
+}
+
+function minus() {
+    if (Number(document.getElementById('vd1').textContent) > 1) {
+        document.getElementById('vd1').innerHTML--;
+    }
+}
+
+
+function fixed_content(){
+    var limit = document.getElementById("sticky-form").getBoundingClientRect().height;
+    var content_height = document.getElementById("fixed-form").getBoundingClientRect().height;
+    var scrollTop = document.documentElement.scrollTop;
+
+    if(scrollTop < limit - content_height){
+        document.getElementById("fixed-form").style.position = "fixed";
+    }
+}
