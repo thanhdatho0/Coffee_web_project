449e91e55abf1874cdf515fca2c7d4f0a0c95157
thanhdatho0 <thanhdatho0@gmail.com>
2023-11-12 22:35:33 +0700

    fix bugfnnaekfaa

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,34 +1,27 @@
 // Change header height when scroll start
-window.onscroll = function(){scrollFunction(), addBoxShadow(),fixed_content()};
+window.onscroll = function () { scrollFunction(), addBoxShadow(), fixed_content() };
 
-function scrollFunction(){
-    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         document.getElementById("header").style.padding = "20px 40px";
     }
-    else{
+    else {
         document.getElementById("header").style.padding = "45px 40px";
     }
 }
 
-function addBoxShadow(){
-    if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
+function addBoxShadow() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         document.getElementById("header").style.boxShadow = "1px 1px 5px #ccc"
     }
-    else{
+    else {
         document.getElementById("header").style.boxShadow = "none";
     }
 }
 // Change header height when scroll end
 
 // Auto slide navigation function start
-var count = 1;
-setInterval(function(){
-    document.getElementById('radio' + count).checked = true;
-    count++;
-    if(count > 3){
-        count = 1;
-    }
-}, 4000);
+
 // Auto slide navigation function end
 
 
@@ -45,34 +38,69 @@ function minus() {
 }
 
 
-function fixed_content(){
+function fixed_content() {
     var limit = document.getElementById("sticky-form").getBoundingClientRect().height;
     var content_height = document.getElementById("fixed-form").getBoundingClientRect().height;
     var scrollTop = document.documentElement.scrollTop;
 
-    if(scrollTop <= limit - (content_height - 30)){
-        if(scrollTop > 50){
+    if (scrollTop <= limit - (content_height - 30)) {
+        if (scrollTop > 50) {
             document.getElementById("fixed-form").style.transform = `translateY(-${30}px)`;
         }
-        else{
+        else {
             document.getElementById("fixed-form").style.transform = "";
         }
         document.getElementById("fixed-form").style.position = "fixed";
         document.getElementById("fixed-form").style.bottom = "";
     }
-    else{
+    else {
         document.getElementById("fixed-form").style.position = "absolute";
         document.getElementById("fixed-form").style.bottom = "-30px";
     }
 }
 
-// Side bar Navigation js start
-
-var closeMenu = document.getElementById("navigation")
-function close_menu(){
-    closeMenu.style.left = "-345px";
+function close_login_form(){
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("Log-in-form").style.right = "-450px";
 }
 
-function open_menu(){
-    closeMenu.style.left = "0";
+function close_sideBar(){
+    document.getElementById("navigation").style.left = "-345px";
 }
+
+$(document).ready(function () {
+    // Open - Close Side Bar
+    $(".menu__button").mouseenter(function () {
+        $.get("Left_sideBar.html", function (data) {
+            $("#navigation").html(data);
+        })
+        $("#navigation").css("left", "0");
+        $("#navigation").mouseleave(function () {
+            $("#navigation").css("left", "-345px");
+        })
+    })
+    // END
+
+    // Open - Close Login Form
+    $(".account-access").click(function () {
+        $.get("Login_form.html", function (data, status) {
+            $("#Log-in-form").html(data);
+        })
+        $("#overlay").css("display", "block");
+        $("#Log-in-form").css("right", "0");
+        $("#overlay").click(function () {
+            $("#overlay").css("display", "none");
+            $("#Log-in-form").css("right", "-450px");
+        })
+    });
+    // END
+});
+
+let count = 1;
+setInterval(function () {
+    document.getElementById('radio' + count).checked = true;
+    count++;
+    if (count > 3) {
+        count = 1;
+    }
+}, 3000);
